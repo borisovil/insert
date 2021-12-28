09803447eb8edd3189833699cf78f35808412cdc
borisovil <74580011+borisovil@users.noreply.github.com>
2021-12-28 13:44:56 +0700

    Add files via upload

---
+++ ConsoleApplication41/ConsoleApplication41/ConsoleApplication41.cpp
@@ -0,0 +1,28 @@
+﻿// ConsoleApplication41.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    char  buf[1024];
+    char tbuf[1024];
+    char* atbuf = tbuf;
+    char masc[] = ".[]{}()";
+    std::cin >> buf;
+    for (int i = 0; buf[i] != '\0'; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (buf[i] == masc[j]) {
+                *atbuf = '\\';
+                atbuf++;
+                break;
+            }
+        }
+        *atbuf = buf[i];
+        atbuf++;
+    }
+    *atbuf = '\0';
+    std::cout <<tbuf <<"\n";
+}
+
